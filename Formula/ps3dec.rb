e21870593220625fab58aa3fe8f037877e362a77
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2025-03-14 22:32:07 +0000

    ps3dec: add 0.2.0 bottle.

--- Formula/ps3dec.rb
+++ Formula/ps3dec.rb
@@ -6,6 +6,11 @@ class Ps3dec < Formula
   version "0.2.0"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/ROpdebee/homebrew-personal/releases/download/ps3dec-0.2.0"
+    sha256 cellar: :any_skip_relocation, ventura: "615f0df7982c5b907b127ac46cca5544da88a40b45eb95d5abc61070f40714df"
+  end
+
   depends_on "rust" => :build
 
   def install

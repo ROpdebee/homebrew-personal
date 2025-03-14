57b48f0f8fe31534bbcd9f43d8a34aed6564c77e
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2025-03-14 18:36:47 +0000

    mpf: add 3.3.0 bottle.

--- Formula/mpf.rb
+++ Formula/mpf.rb
@@ -5,6 +5,11 @@ class Mpf < Formula
   sha256 "ffa128efe46f69cdb8b71f59471ac9c8620793f24961af6e0ca983b624ab02e7"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/ROpdebee/homebrew-personal/releases/download/mpf-3.3.0"
+    sha256 cellar: :any, ventura: "b2fc03a71b09caa0c161fe5544c8042fa9975d42017fd9193a47a78b76725579"
+  end
+
   depends_on "dotnet" => :build
   depends_on "brotli"
 

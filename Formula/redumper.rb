0329d3acc182d2eda1d19d5553bbfbe4b937ea25
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2026-07-27 01:41:37 +0200

    redumper 735
    Closes #16.
    
    Signed-off-by: Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>

--- Formula/redumper.rb
+++ Formula/redumper.rb
@@ -1,8 +1,8 @@
 class Redumper < Formula
   desc "Low level CD dumper utility"
   homepage "https://github.com/superg/redumper"
-  url "https://github.com/superg/redumper/archive/refs/tags/build_503.tar.gz"
-  sha256 "62116a9f295e260fedfcb433e50b5e3d100a7c27a2a8df3e25fc1b6f398db8c1"
+  url "https://github.com/superg/redumper/archive/refs/tags/b735.tar.gz"
+  sha256 "8021434ac3e714bc81c32d6c74d9ecf789d7c2ba80fd776b1995ffdacb718317"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -11,26 +11,30 @@ class Redumper < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "llvm" => :build
+  depends_on "llvm@18" => :build
   depends_on "ninja" => :build
+
+  resource "googletest" do
+    url "https://github.com/google/googletest/archive/refs/tags/v1.15.2.tar.gz"
+    sha256 "7b42b4d6ed48810c5362c265a17faebe90dc2373c885e5216439d37927f02926"
+  end
 
   def llvm
     deps.map(&:to_formula).find { |f| f.name.match?(/^llvm(@\d+)?$/) }
   end
 
   def install
+    resource("googletest").stage buildpath/"googletest-src"
+
     cmake_args = %W[
       -G Ninja
       -DREDUMPER_CLANG_USE_LIBCPP=ON
       -DREDUMPER_VERSION_BUILD=#{version}
+      -DFETCHCONTENT_SOURCE_DIR_GOOGLETEST=#{buildpath}/googletest-src
+      -DLLVM_LIB_PATH=#{llvm.opt_lib}
     ]
 
     ENV["CXX"] = llvm.opt_bin/"clang++"
-
-    # Patch sources to recognise our drive
-    inreplace "drive.ixx",
-      "\"BD-RE BH16NS55\"  , \"1.02\", \"N000200SIK92G9OF211\"",
-      "\"BD-RE BH16NS55\"  , \"1.05\", \"N001601KL1O1JB1603\""
 
     system "cmake", "-S", ".", "-B", "build", *cmake_args, *std_cmake_args
     system "cmake", "--build", "build"
@@ -40,6 +44,6 @@ class Redumper < Formula
 
   test do
     system bin/"redumper", "--help"
-    assert_match "error: no ready drives detected on the system", shell_output("#{bin}/redumper cd 2>&1", 255)
+    assert_match "error: no ready drives detected on the system", shell_output("#{bin}/redumper disc 2>&1", 255)
   end
 end
